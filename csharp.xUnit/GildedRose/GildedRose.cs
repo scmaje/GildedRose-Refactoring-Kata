93aadad1e503e48edd75c0303e1ec10511a8bda9
Steve Majewski <smajewski@amerisure.com>
2025-05-12 20:55:10 -0600

    Refactored and added functionality.

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -1,89 +1,115 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace GildedRoseKata;
 
-public class GildedRose
+public class GildedRose(
+    IList<Item> items, 
+    HashSet<string> ignoredItems = null, 
+    Action<Item> defaultUpdateQualityHandler = null, 
+    Dictionary<string, Action<Item>> customUpdateQualityHandlers = null
+)
 {
-    IList<Item> Items;
+    const int QUALITY_MIN = 0;
+    const int QUALITY_MAX = 50;
 
-    public GildedRose(IList<Item> Items)
+    readonly IList<Item> Items = items;
+
+    readonly HashSet<string> IgnoredItems = ignoredItems ??
+    [
+        "Sulfuras, Hand of Ragnaros"
+    ];
+
+    readonly Action<Item> DefaultUpdateQualityHandler = defaultUpdateQualityHandler ?? (item =>
     {
-        this.Items = Items;
-    }
+        UpdateQualityBy(item, -1);
+        // degrade quality twice as fast when expired
+        if (item.SellIn < 0)
+            UpdateQualityBy(item, -1);
+    });
+
+    readonly Dictionary<string, Action<Item>> CustomUpdateQualityHandlers = customUpdateQualityHandlers ?? new()
+    {
+        // aged brie
+        // - improves with age
+        ["Aged Brie"] = item =>
+        {
+            if (item.Quality < 50)
+            {
+                UpdateQualityBy(item, 1);
+                // quality boost for really old cheese
+                if (item.SellIn < 0)
+                    UpdateQualityBy(item, 1);
+            }
+        },
+
+        // backstage pass
+        // - improves with age
+        // - doubles with under 10 days left
+        // - triples with under 5 days left
+        ["Backstage passes to a TAFKAL80ETC concert"] = item =>
+        {
+            if (item.SellIn < 0)
+            {
+                // sellin date passed, tickets are worthless
+                item.Quality = 0;
+            }
+            else if (item.Quality < 50)
+            {
+                UpdateQualityBy(item, 1);
+                // quality boost under 10 days out
+                if (item.SellIn < 10)
+                    UpdateQualityBy(item, 1);
+                // quality boost under 5 days out
+                if (item.SellIn < 5)
+                    UpdateQualityBy(item, 1);
+            }
+        },
+
+        // conjured item
+        // - degrades twice as fast
+        ["Conjured Mana Cake"] = item =>
+        {
+            UpdateQualityBy(item, -2);
+            if (item.SellIn < 0)
+                UpdateQualityBy(item, -2);
+        }
+    };
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < Items.Count; i++)
+        // snapshot items
+        Item[] snapshot;
+        lock (Items)
+            snapshot = [.. Items];
+
+        // parallel process items
+        Parallel.ForEach(snapshot, UpdateQuality);
+    }
+
+    private void UpdateQuality(Item item)
+    {
+        if (!IgnoredItems.Contains(item.Name))
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            lock (item)
             {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
+                // reduce sell days remaining
+                item.SellIn--;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                // lookup update quality handler for this item
+                var updateQualityHandler = CustomUpdateQualityHandlers
+                                                .GetValueOrDefault(
+                                                    item.Name, 
+                                                    DefaultUpdateQualityHandler
+                                                );
 
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
+                // invoke update quality handler
+                updateQualityHandler(item);
             }
         }
     }
+
+    private static void UpdateQualityBy(Item item, int value) =>
+        item.Quality = Math.Clamp(item.Quality + value, QUALITY_MIN, QUALITY_MAX);
 }
